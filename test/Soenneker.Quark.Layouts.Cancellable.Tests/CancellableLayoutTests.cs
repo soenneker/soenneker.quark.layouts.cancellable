362a2c600afd9fce272d47e9894b724341cd7242
Jake Soenneker <jake@soenneker.com>
2025-08-11 19:12:00 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Layouts.Cancellable.Tests/CancellableLayoutTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Layouts.Cancellable.Tests;
+
+public sealed class CancellableLayoutTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}

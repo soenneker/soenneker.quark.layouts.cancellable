25267f974bfdb0c477a0a371a6717cc63198ff6c
Jake Soenneker <jake@soenneker.com>
2025-09-06 08:21:28 -0500

    Server usage

--- src/CancellableLayout.cs
+++ src/CancellableLayout.cs
@@ -29,7 +29,7 @@ public abstract class CancellableLayout : LayoutComponentBase, ICancellableLayou
             {
                 try
                 {
-                    await cts.CancelAsync().NoSync();
+                    await cts.CancelAsync();
                 }
                 catch
                 {
